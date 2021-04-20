1dd19f6769e428b91ec8369f7b395f9391109202
bunnyDrug <1652666+bunnyDrug@users.noreply.github.com>
2021-04-21 00:26:41 +0100

    initial commit

---
+++ Day_1/main.py
@@ -0,0 +1,7 @@
+def main():
+    txt = open('input.txt').readlines()
+    values = [(int(x) * int(y)) for x in txt for y in txt if int(x) + int(y) == 2020]
+    result = values.pop()
+    print(result)
+
+main()
